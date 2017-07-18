5a601bb606f3c5a8fab6914cf9514aa38ecc348e
zhjingh2 <zhjingh2@mail2.sysu.edu.cn>
2017-07-18 15:40:40 +0800

    JsonParser类定义

--- jsonparser.py
+++ jsonparser.py
@@ -1,1 +1,24 @@
-print("test 2nd")
+class JsonParser:
+    def __init__(self, _data):
+        self._data = _data
+
+    def loads(self, s):
+        pass
+
+    def dumps(self):
+        pass
+
+    def load_file(self, f):
+        pass
+
+    def dump_file(self,f):
+        pass
+
+    def load_dict(self, d):
+        pass
+
+    def dump_dict(self):
+        pass
+
+jp = JsonParser("abc")
+print(jp)
